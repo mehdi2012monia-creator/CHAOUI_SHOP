8f1063bc9e88187c96ca71d669006454d1f3df2f
mehdi2012monia-creator <mehdi2012monia@gmail.com>
2026-08-25 21:39:46 +0100

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 ```ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
 ```
