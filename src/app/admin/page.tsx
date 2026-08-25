34d9fa6346e98e4091743f8c38f4136d70aa6079
mehdi2012monia-creator <mehdi2012monia@gmail.com>
2026-08-25 21:37:23 +0100

    page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,9 @@
-"use client";
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
+```tsx
 import { AdminPanel } from "./panel";
+
+export const revalidate = 0;
 
 export default function AdminPage() {
   return <AdminPanel />;
 }
+```
